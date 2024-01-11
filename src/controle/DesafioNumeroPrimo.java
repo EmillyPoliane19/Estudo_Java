c600f9843dec0e1affd3af4e7aa9081d970bd38a
EmillyPoliane19 <emilly.poliane19@gmail.com>
2024-01-10 21:54:19 -0300

    Exercicio 4 do modulo de controle

---
+++ src/controle/DesafioNumeroPrimo.java
@@ -0,0 +1,30 @@
+package src.controle;
+
+import java.util.Scanner;
+
+public class DesafioNumeroPrimo {
+    public static void main(String[] args) {
+        //Criar um programa que receba um número e diga se ele é um número primo
+        //Entrada
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int numero = sc.nextInt();
+
+        //Processamento
+        int quantDivisores = 0;
+        for (int i = 2; i < numero;i++){
+            if (numero % i == 0){
+                quantDivisores ++;
+            }
+        }
+
+        if (quantDivisores != 0){
+            System.out.println("O número " + numero + " não é primo");
+
+        } else {
+            System.out.printf("O número " + numero + " é primo!");
+        }
+        sc.close();
+    }
+}
