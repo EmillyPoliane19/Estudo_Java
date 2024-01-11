fd72a133c41cd774ebfdbff1ab98c5fdfd9ec171
EmillyPoliane19 <emilly.poliane19@gmail.com>
2024-01-11 11:41:10 -0300

    Exercicio 8 do módulo de controle, utilizando for

---
+++ src/controle/DesafioPalavra.java
@@ -0,0 +1,21 @@
+package src.controle;
+
+import java.util.Scanner;
+
+public class DesafioPalavra {
+    public static void main(String[] args) {
+        /*Criar um programa que receba uma palavra e imprime no console letra por letra.*/
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escreva uma palavra:");
+        String palavra = sc.nextLine();
+
+        char letras[] = palavra.toCharArray();
+
+        for (char i = 0; i < letras.length; i++){
+            System.out.println(letras[i]);
+        }
+
+        sc.close();
+    }
+}
