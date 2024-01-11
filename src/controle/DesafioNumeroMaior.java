0287e6e11db3a6ef0a4b98e2edc5aab8c02b6b94
EmillyPoliane19 <emilly.poliane19@gmail.com>
2024-01-11 11:41:33 -0300

    Exercicio 9 do módulo de controle, utilizando for

---
+++ src/controle/DesafioNumeroMaior.java
@@ -0,0 +1,22 @@
+package src.controle;
+
+import java.util.Scanner;
+
+public class DesafioNumeroMaior {
+    public static void main(String[] args) {
+        /*Crie um programa que recebe 10 valores e ao final imprima o maior número.*/
+        Scanner sc = new Scanner(System.in);
+
+        int numeroMaior = 0;
+        int numero = 0;
+
+        for (int i = 0;i < 10; i++){
+            System.out.printf("Digite o %dº número:%n", (i + 1));
+            numero = sc.nextInt();
+            if (numero > numeroMaior){
+                numeroMaior = numero;
+            }
+        }
+        System.out.println("O maior número é " + numeroMaior);
+    }
+}
